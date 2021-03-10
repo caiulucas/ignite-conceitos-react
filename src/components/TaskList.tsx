c2ef0feb414c0d02c3255e12d22c9832374dc0ec
Caio Lucas <lienscarlet@gmail.com>
2021-03-10 13:58:32 -0300

    challenge complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,30 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle) {
+      const task: Task = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      };
+      console.log(task);
+
+      setTasks([...tasks, task]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editedTasks = tasks.map(task => {
+      task.id === id && (task.isComplete = true);
+      return task;
+    });
+    
+    setTasks(editedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => (task.id !== id));      
+    setTasks(filteredTasks);
   }
 
   return (
